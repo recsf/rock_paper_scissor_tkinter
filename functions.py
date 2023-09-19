1c4dc6422f918f5dc6b6600964b1c4a5de4344f5
recsf <79220184+recsf@users.noreply.github.com>
2023-09-19 09:36:31 -0600

    Completed

--- functions.py
+++ functions.py
@@ -1,2 +1,34 @@
-def spin():
-    pass
+from random import randint
+from tkinter.messagebox import showinfo
+
+
+def spin(value, root):
+    choose_number = randint(0, 2)
+    human = 0
+    if value == "Rock":
+        human = 0
+    elif value == "Paper":
+        human = 1
+    elif value == "Scissors":
+        human = 2
+    if human == 0:
+        if choose_number == 0:
+            showinfo("", "Tie! - "+" Computer: Bad luck")
+        elif choose_number == 1:
+            showinfo("", "YOU Loose - "+" Computer: I am better")
+        elif choose_number == 2:
+            showinfo("", "YOU Won - "+" Computer: You won by luck")
+    elif human == 1:
+        if choose_number == 1:
+            showinfo("", "Tie! - "+" Computer: Bad luck")
+        elif choose_number == 0:
+            showinfo("", "YOU Loose - "+" Computer: I am better")
+        elif choose_number == 2:
+            showinfo("", "YOU Won - "+" Computer: You won by luck")
+    elif human == 2:
+        if choose_number == 2:
+            showinfo("", "Tie! - "+" Computer: Bad luck")
+        elif choose_number == 0:
+            showinfo("", "YOU Loose - "+" Computer: I am better")
+        elif choose_number == 1:
+            showinfo("", "YOU Won - "+" Computer: You won by luck")
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter import ttk
-from random import *
 from functions import spin
 
 root = Tk()
@@ -9,7 +8,6 @@ root.geometry("500x500")
 root.title("Rock-Paper-Scissors-Game")
 
 choices = ["rock", "paper", "scissors"]
-choose_number = randint(0, 2)
 
 label = Label(root, text="Select your choice", width=20, height=4, font=("calibri", 15))
 label.pack()
@@ -18,9 +16,7 @@ user_select = ttk.Combobox(root, values=["Rock", "Paper", "Scissors"])
 user_select.current(0)
 user_select.pack()
 
-wl_label = Label(root, text="", font=("arial", 10), width=50, height=4)
-wl_label.pack()
-button = Button(root, text="Spin", font=("bell mt", 10), command=spin)
+button = Button(root, text="Spin", font=("bell mt", 10), command=lambda: spin(user_select.get(), root))
 button.pack()
 
 root.mainloop()
